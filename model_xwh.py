d501aa624126d461f945627ba4162e38d7619620
widiantoko <dodiwidiantoko@gmail.com>
2025-01-06 20:46:41 +0700

    test

--- model_xwh.py
+++ model_xwh.py
@@ -96,7 +96,7 @@ with colA:
 
   legend={'loc': 'upper left', 'bbox_to_anchor': (1.05, 1),'fontsize':10, },
   figsize=(9, 6),
-  icons= 'bottle-water' , icon_size=14, icon_legend=True,
+  icons= 'fi fi-ss-water-bottle' , icon_size=14, icon_legend=True,
   title= {'label': 'Komposisi dan Jumlah Produk di Gudang Kedep', 'loc': 'left', 'fontsize':12, 'weight':'bold', })
 
 
